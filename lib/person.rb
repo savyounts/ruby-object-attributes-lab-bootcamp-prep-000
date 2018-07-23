74ea8d1d47f51c85ec8b2068651e708d3a8e645b
Sav Younts <savyounts@gmail.com>
2018-07-23 13:28:58 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,8 @@
 class Person
+  def initialize (name, job)
+    @name = name
+    @job = job
+  end
   
   def name=(new_name)
     @name = new_name
@@ -6,5 +10,13 @@ class Person
   
   def name 
     "#{@name}"
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
+  
+  def job
+    "#{@job}"
   end
 end 
