f8b31c5d55f07e03583ca32b59fed3627c242b16
Sav Younts <savyounts@gmail.com>
2018-07-23 13:18:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,13 +5,15 @@ class Dog
     @breed = dog_breed
   end
   
+  def name=(new_name)
+   @name = new_name
+  end
+  
   def name
     @name 
   end
   
-  def name=(new_name)
-   @name = new_name
-  end
+  
   
   def breed (dog_breed)
     @breed
